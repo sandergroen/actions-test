88dacfabfb3f19208047b92c2073af9b0ff49611
Sander Groen <sander.groen@topicus.nl>
2024-08-29 16:11:46 +0200

    Load files from spec/support

--- spec/support/driver.rb
+++ spec/support/driver.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]  do |driver_options|
+      driver_options.add_argument("--disable-site-isolation-trials")
+      driver_options.add_argument('--disable-search-engine-choice-screen')
+    end
   end
 end
